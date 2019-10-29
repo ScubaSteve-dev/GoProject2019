36d77728c093c60261854da87a6cb1a83af0104c
Beyjo <Beyjo@10.30.121.246>
2019-10-29 11:36:17 -0500

    added some of Player class

--- src/Game.java
+++ src/Game.java
@@ -1,17 +1,44 @@
 
 public class Game
 {
-	public static Player black, white;
-	public static long TimeLimit;
-	public static boolean GameOver, BlackPlayerTurn;
-	public static int NumberPasses;
-	public static Board board;
-	public static GameBoardUI BoardWindow;
+	public Player black, white;
+	public long TimeLimit;
+	public boolean GameOver, BlackPlayerTurn;
+	public int NumberPasses= 0;
+	public Board board;
+	public GameBoardUI BoardWindow;
+	public boolean move() {
+		while (NumberPasses !=2)
+		{
+	}
+		return GameOver;
+}
+	
+	public void Start() {
+		while (!GameOver) 
+		{
+			
+		}
+		
+	}
 	public Game (GameBoardUI B) {
 		BoardWindow= B;
 		black=new Player();
 		white=new AI();
 		board=new Board(B,black,white);
 		
+		
 	}
+	public void run() {
+	
+		Player toMove = black;
+		int skip= NumberPasses;
+	
+		while (skip<2) {
+			skip= toMove.move() ? 0 : skip + 1;
+	}
+	
+	}
+	
+	
 }
--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,17 @@
 
 public class Player
 {
+	public String playerName;
+	public int piecesLeft;
 	
+	public Player()
+	{
+		playerName = "Player";
+		piecesLeft = 181;
+	}
+
+	public boolean move() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
