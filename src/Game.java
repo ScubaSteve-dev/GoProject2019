72c647c212d7490011768cdb77c8e6917a09d327
macbook <macbook@10.30.121.237>
2019-10-29 11:16:08 -0500

    First implementation of game class

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,17 @@
 
 public class Game
 {
-	
+	public static Player black, white;
+	public static long TimeLimit;
+	public static boolean GameOver, BlackPlayerTurn;
+	public static int NumberPasses;
+	public static Board board;
+	public static GameBoardUI BoardWindow;
+	public Game (GameBoardUI B) {
+		BoardWindow= B;
+		black=new Player();
+		white=new AI();
+		board=new Board(B,black,white);
+		
+	}
 }
