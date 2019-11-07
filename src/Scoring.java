8a27d0c5ff5cb292a3c7669f5c5b1192e09f387e
Manjul Shrestha <manjulshr@gmail.com>
2019-11-07 10:39:02 -0600

    Addition of simple logic to check the surrounding, recursion to be added later.

--- src/Scoring.java
+++ src/Scoring.java
@@ -1,52 +1,91 @@
-import java.awt.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Scoring{
+public class Scoring {
 
-    public int scoreBlack=0, scoreWhite=0;
-    ArrayList<Character> traversedCharacter;
+    public int scoreBlack = 0, scoreWhite = 0;
     char[][] currentBoard;
 
-    public void calculateScore(char[][] currentBoard){
-        this.currentBoard=currentBoard;
+    public void calculateScore(char[][] currentBoard) {
+        this.currentBoard = currentBoard;
         int columns = currentBoard.length;
         int rows = currentBoard[0].length;
-        for (int i=0;i<rows;i++){
-            for (int j=0;j<columns;j++){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                try {
+                    if (checkUp(i, j) == 'B') {
+                        if (checkRight(i, j) == 'B') {
+                            if (checkDown(i, j) == 'B') {
+                                if (checkLeft(i, j) == 'B') {
+                                    scoreBlack++;
+                                } else {
+                                    break;
+                                }
+                            }else if(checkDown(i, j) == ' '){
 
+                            }else{
+                                break;
+                            }
+                        }else if(checkRight(i, j) == ' '){
+
+                        }else{
+                            break;
+                        }
+                    } else if (checkUp(i, j) == 'W') {
+                        if (checkRight(i, j) == 'W') {
+                            if (checkDown(i, j) == 'W') {
+                                if (checkLeft(i, j) == 'W') {
+                                    scoreWhite++;
+                                } else {
+                                    break;
+                                }
+                            }else if(checkDown(i, j) == ' '){
+
+                            }else{
+                                break;
+                            }
+                        }else if(checkRight(i, j) == ' '){
+
+                        }else{
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                } catch (Exception e) {
+                    //e.printStackTrace();
+                }
             }
         }
+    }
 
+    public char checkUp(int x, int y) {
+        return currentBoard[x][y - 1];
+    }
 
+    public char checkDown(int x, int y) {
+        return currentBoard[x][y + 1];
+    }
+
+    public char checkRight(int x, int y) {
+        return currentBoard[x + 1][y];
 
     }
 
-    public char checkUp(int x, int y){
-        return currentBoard[x][y];
-    }
-
-    public char checkDown(int x, int y){
-        return currentBoard[x][y];
-    }
-
-    public char checkRight(int x, int y){
-        return currentBoard[x][y];
-
-    }
-    public char checkLeft(int x, int y){
-        return currentBoard[x][y];
+    public char checkLeft(int x, int y) {
+        return currentBoard[x - 1][y];
 
     }
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
         char[][] currentBoard = new char[19][19];
-        for (char[] c : currentBoard){
+        for (char[] c : currentBoard) {
             Arrays.fill(c, ' ');
         }
-        Scoring scoring=new Scoring();
+        Scoring scoring = new Scoring();
         scoring.calculateScore(currentBoard);
+        System.out.println("White:"+scoring.scoreWhite);
+        System.out.println("Black:"+scoring.scoreBlack);
     }
 
     public int getScoreBlack() {
